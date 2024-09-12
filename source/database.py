501cb520e0ee0b6fa405b3aa7f3bede359352db3
kofiol <psyhik17@gmail.com>
2024-09-12 19:34:41 +0200

    added the database thing, next step is to make it remote and mublic (i will use mysql i think)

--- source/database.py
+++ source/database.py
@@ -1,1 +1,37 @@
-# after done with prototyping using json files which are not secure, will add a sql/postgresql database to store hashes
+import sqlite3
+
+
+def connect_db():
+    return sqlite3.connect('hashes.db')
+
+
+def create_table():
+    with connect_db() as conn:
+        cursor = conn.cursor()
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS hashes (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                hash TEXT UNIQUE
+            )
+        ''')
+        conn.commit()
+
+
+def hash_exists(hash):
+    with connect_db() as conn:
+        cursor = conn.cursor()
+        cursor.execute('SELECT 1 FROM hashes WHERE hash = ?', (hash,))
+        return cursor.fetchone() is not None
+
+
+def add_hash(hash):
+    with connect_db() as conn:
+        cursor = conn.cursor()
+        try:
+            cursor.execute('INSERT INTO hashes (hash) VALUES (?)', (hash,))
+            conn.commit()
+        except sqlite3.IntegrityError:
+            pass
+
+
+create_table()
