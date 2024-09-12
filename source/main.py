2e4339f4315451e40ca7fada14bbdea62158dcdb
kofiol <psyhik17@gmail.com>
2024-09-12 18:15:41 +0200

    made the hashes logic work properly and i think it is finished, next step is to turn json into database. also made a dictionary from 64 words so it adds to the password so you can organize and even remeber it

--- source/main.py
+++ source/main.py
@@ -5,34 +5,66 @@ from colorama import just_fix_windows_console
 just_fix_windows_console()
 import hashlib
 import json
+import math
+
 
 with open('hashes.json') as file:
     data = json.load(file)
+with open('dictionary.txt') as dict_file:
+    dictionary = dict_file.read().splitlines()
 
-print("this is a password generator that never repeats itself!")
+
+print("this is a password generator that never repeats itself! it also adds a word to the end or the start of the password so you can easily organize and even memorize it!")
+
 
 symbols_amount = int(input("now select the amount of symbols you want to generate (we recommend 15-25 symbols): "))
 
-alphabet = string.ascii_letters + string.digits
+alphabet = string.ascii_letters + string.digits + string.punctuation
+
+
+while True:
+    additional_word = secrets.choice(dictionary)
+    if 3 <= len(additional_word) <= 7:
+        break
+
+
+symbols_amount = max(1, symbols_amount - len(additional_word))
+
+
 while True:
     password = ''.join(secrets.choice(alphabet) for i in range(symbols_amount))
+    password = password + additional_word if secrets.choice([True, False]) else additional_word + password
     if (any(c.islower() for c in password)
             and any(c.isupper() for c in password)
+            and any(c in string.punctuation for c in password)
             and sum(c.isdigit() for c in password) >= 3):
         break
 
-hash_object = hashlib.sha256()
-hash_object.update(password.encode('utf-8'))
-hash = hash_object.hexdigest()
 
-print(colorama.Fore.GREEN + password + colorama.Style.RESET_ALL)
-print(colorama.Fore.RED + hash)
-print(colorama.Fore.BLUE + data['1'] + colorama.Style.RESET_ALL)
+while True:
+    hash_object = hashlib.sha256()
+    hash_object.update(password.encode('utf-8'))
+    hash = hash_object.hexdigest()
+    if hash in data.values():
+        print(f"looks like the hash is already present in the database, generating a new one.")
+        while True:
+            additional_word = secrets.choice(dictionary)
+            if 3 <= len(additional_word) <= 7:
+                break
 
-if hash in data.values():
-    print(f"Looks like the hash is already present in the database, make a new one.")
-else:
-    print(f"The password is not present in the database, you can use it.")
+
+        symbols_amount = int(input("now select the amount of symbols you want to generate (we recommend 15-25 symbols): ")) - len(additional_word)
+        symbols_amount = max(1, symbols_amount)  
+        password = ''.join(secrets.choice(alphabet) for i in range(symbols_amount))
+        password = password + additional_word if secrets.choice([True, False]) else additional_word + password
+    else:
+        print(f"the password is not present in the database, you can use it.")
+        break
+
+
+print("your password is: " + colorama.Fore.GREEN + password + colorama.Style.RESET_ALL)
+print("advise: better save it on paper or at least in a reliable password manager!")
+
 
 last_var = max(map(int, data.keys()))
 next_var = last_var + 1
